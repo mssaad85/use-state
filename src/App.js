4753debc6d21093d89c2576a3874ddd651de8d36
mssaad85 <mssaad85@users.noreply.github.com>
2020-12-20 01:19:08 +0500

    use state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Message from "./Message.js";
+import "./App.css";
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [ismorning, isnight] = useState(1)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ={`box ${ismorning ? 'ismorning' : ''} `} >
+       <h1>day time = {ismorning ? 'morning' : 'night' }</h1> 
+      <Message counter ={count}/>
+      <button onClick={() => setCount(++count)}>add num</button>
+      <button onClick={() => setCount(count=0)}>reset</button>
+      <br></br>
+      <button onClick = {()=> isnight (!ismorning) }> set day</button>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Message.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Message (props){
+    return(
+        <div>
+            <h3>Value of coutner variavle is : {props.counter}</h3>
+        </div>
+    )
+}
+export default Message;
